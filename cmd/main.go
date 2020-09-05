116ca36d388192f03bda6d05d663f8d9c3d84dc2
prajat <rajat.singh1084@gmail.com>
2020-09-06 00:12:15 +0530

    api made

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
+	"net/http"
 
 	"github.com/go-chi/chi"
 )
@@ -11,4 +14,12 @@ func main() {
 	fmt.Println("starting server....")
 
 	router := chi.NewRouter()
+	router.Get("/api/getExample", getHandler)
+	fmt.Println("server is listening to port 8080")
+	log.Fatal(http.ListenAndServe(":8080", router))
+
+}
+func getHandler(w http.ResponseWriter, r *http.Request) {
+
+	json.NewEncoder(w).Encode("you got me")
 }
