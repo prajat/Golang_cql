6ca760d4a0bf2ce66f73273410901dc304e30938
prajat <rajat.singh1084@gmail.com>
2020-09-06 00:24:28 +0530

    post request done

--- cmd/main.go
+++ cmd/main.go
@@ -15,6 +15,8 @@ func main() {
 
 	router := chi.NewRouter()
 	router.Get("/api/getExample", getHandler)
+	router.Post("/api/postExample", postHandler)
+
 	fmt.Println("server is listening to port 8080")
 	log.Fatal(http.ListenAndServe(":8080", router))
 
@@ -22,4 +24,8 @@ func main() {
 func getHandler(w http.ResponseWriter, r *http.Request) {
 
 	json.NewEncoder(w).Encode("you got me")
+}
+func postHandler(w http.ResponseWriter, r *http.Request) {
+
+	json.NewEncoder(w).Encode("you sent me the post request")
 }
